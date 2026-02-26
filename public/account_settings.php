03c285aebe0c5231efce85828cb633ced0b4b75c
Sekaus <christian.foged.ludvigsen@hotmail.com>
2026-02-26 11:43:13 +0100

    Update account_settings.php

--- public/account_settings.php
+++ public/account_settings.php
@@ -20,7 +20,7 @@
         <?php include_once("./setup.php"); ?>
         <?php include_once("./navigation_bar.php"); ?>
 
-        <form action="" method="POST">
+        <div>
             <div id="account-settings">
                 <div id="account-settings-box">
                     <div class="account-settings-input-box">
@@ -31,7 +31,7 @@
                                 class="account-settings-input" />
                         </div>
                     </div>
-                    <hr />
+                    <hr/>
                     <div class="account-settings-input-box">
                         <label>Name:</label>
                         <div>
@@ -40,7 +40,7 @@
                             <strong>This field is private</strong>
                         </div>
                     </div>
-                    <hr />
+                    <hr/>
                     <div class="account-settings-input-box">
                         <label>Password:</label>
                         <div>
@@ -49,7 +49,7 @@
                             <strong>This field is private</strong>
                         </div>
                     </div>
-                    <hr />
+                    <hr/>
                     <div class="account-settings-input-box">
                         <label>Email:</label>
                         <div>
@@ -58,15 +58,34 @@
                             <strong>This field is private</strong>
                         </div>
                     </div>
-                    <hr />
+                    <hr/>
+                    <div class="account-settings-input-box">
+                        <label>Land:</label>
+                        <div>
+                            <input id="land-input" name="land" type="text" placeholder="Germany"
+                                class="account-settings-input" />
+                            <b>Is this field is private?</b><input type="checkbox" id="land-visibility-checkbox" name="land-visibility"/>
+                        </div>
+                    </div>
+                    <hr/>
                     <div class="account-settings-input-box">
                         <label>Date of birth:</label>
                         <div>
                             <input id="date-of-birth-input" name="date-of-birth" type="text" placeholder="11/03/1998"
                                 class="account-settings-input" />
+                            <b>Is this field is private?</b><input type="checkbox" id="date-of-birth-visibility-checkbox" name="date-of-birth-visibility"/>
                         </div>
                     </div>
-                    <hr />
+                    <hr/>
+                    <div class="account-settings-input-box">
+                        <label>Gender:</label>
+                        <div>
+                            <input id="gender-input" name="gender" type="text" placeholder="Non-binary"
+                                class="account-settings-input" />
+                            <b>Is this field is private?</b><input type="checkbox" id="gender-visibility-checkbox" name="gender-visibility"/>
+                        </div>
+                    </div>
+                    <hr/>
                     <div class="account-settings-input-box">
                         <label>Username:</label>
                         <div>
@@ -74,7 +93,7 @@
                                 class="account-settings-input" />
                         </div>
                     </div>
-                    <hr />
+                    <hr/>
                     <div class="account-settings-input-box">
                         <label>Tagline:</label>
                         <div>
@@ -82,7 +101,7 @@
                                 class="account-settings-input">So cool!</textarea>
                         </div>
                     </div>
-                    <hr />
+                    <hr/>
                     <div class="account-settings-input-box">
                         <label>Hobbies:</label>
                         <div>
@@ -90,6 +109,7 @@
                                 class="account-settings-input">Gameing/Painting/Tagging</textarea>
                         </div>
                     </div>
+                    <hr/>
                     <div class="account-settings-input-box">
                         <label>BIO:</label>
                         <div>
@@ -107,7 +127,7 @@
 
                 <div class="vertical-hr"></div>
             </div>
-        </form>
+        </div>
 
         <?php include_once("./html_elements/footer.html"); ?>
     </body>
@@ -118,9 +138,13 @@
             icon: '<?php echo $dh->GetURLOnSingleFile($user->profileImage()) ?? "../images/default_pp.webp"; ?>',
             name: '<?php echo $unfilteredData["name"] ?? ""; ?>',
             email: '<?php echo $login->email(); ?>',
+            land: '<?php echo $unfilteredData["land"] ?? ""; ?>',
+            landVisible: <?php echo $user->landVisible() ? "true" : "false"; ?>,
             dateOfBirth: '<?php echo $unfilteredData["dateOfBirth"] ?? ""; ?>',
+            dateOfBirthVisible: <?php echo $user->dateOfBirthVisible() ? "true" : "false"; ?>,
+            gender: '<?php echo $unfilteredData["gender"] ?? ""; ?>',
+            genderVisible: <?php echo $user->genderVisible() ? "true" : "false"; ?>,
             username: '<?php echo $user->username(); ?>',
-            dateOfBirth: '<?php echo $user->dateOfBirth(); ?>',
             tagline: '<?php echo $user->tagline(); ?>',
             hobbies: '<?php echo $user->hobbies(); ?>',
             bio: '<?php echo $user->biography(); ?>'
@@ -129,10 +153,80 @@
         $(".user-icon").attr("src", user.icon);
         $("#name-input").val(user.name);
         $("#email-input").val(user.email);
+        $("#land-input").val(user.land);
+        $("#land-visibility-checkbox").prop("checked", user.landVisible);
         $("#date-of-birth-input").val(user.dateOfBirth);
+        $("#date-of-birth-visibility-checkbox").prop("checked", user.dateOfBirthVisible);
+        $("#gender-input").val(user.gender);
+        $("#gender-visibility-checkbox").prop("checked", user.genderVisible);
         $("#username-input").val(user.username);
         $("#tagline-input").val(user.tagline);
         $("#hobbies-input").val(user.hobbies);
         $("#bio-input").val(user.bio);
+
+        // When the cancel button is clicked, reset the form fields to their original values.
+        $("#cancel").click(function() {
+            // Reset the form fields to their original values.
+            $("#name-input").val(user.name);
+            $("#email-input").val(user.email);
+            $("#land-input").val(user.land);
+            $("#land-visibility-checkbox").prop("checked", user.landVisible);
+            $("#date-of-birth-input").val(user.dateOfBirth);
+            $("#date-of-birth-visibility-checkbox").prop("checked", user.dateOfBirthVisible);
+            $("#gender-input").val(user.gender);
+            $("#gender-visibility-checkbox").prop("checked", user.genderVisible);
+            $("#username-input").val(user.username);
+            $("#tagline-input").val(user.tagline);
+            $("#hobbies-input").val(user.hobbies);
+            $("#bio-input").val(user.bio);
+        });
+
+        $(".submit").click(function() {
+            // Get the values from the form fields.
+            var name = $("#name-input").val();
+            var email = $("#email-input").val();
+            var land = $("#land-input").val();
+            var landVisible = $("#land-visibility-checkbox").is(":checked");
+            var dateOfBirth = $("#date-of-birth-input").val();
+            var dateOfBirthVisible = $("#date-of-birth-visibility-checkbox").is(":checked");
+            var gender = $("#gender-input").val();
+            var genderVisible = $("#gender-visibility-checkbox").is(":checked");
+            var username = $("#username-input").val();
+            var tagline = $("#tagline-input").val();
+            var hobbies = $("#hobbies-input").val();
+            var bio = $("#bio-input").val();
+
+            // Send the updated data to the server using AJAX.
+            $.ajax({
+                url: "settings_handler.php",
+                method: "POST",
+                data: {
+                    command: "update_profile",
+                    name: name,
+                    email: email,
+                    land: land,
+                    landVisible: landVisible,
+                    dateOfBirth: dateOfBirth,
+                    dateOfBirthVisible: dateOfBirthVisible,
+                    gender: gender,
+                    genderVisible: genderVisible,
+                    username: username,
+                    tagline: tagline,
+                    hobbies: hobbies,
+                    bio: bio
+                },
+                success: function(response) {
+                    if(response.success) {
+                        alert("Profile updated successfully!");
+                        location.reload(); // Reload the page to reflect the changes.
+                    } else {
+                        alert("Error updating profile: " + response.error);
+                    }
+                },
+                error: function() {
+                    alert("An error occurred while updating your profile. Please try again later.");
+                }
+            });
+        });
     </script>
 </html>

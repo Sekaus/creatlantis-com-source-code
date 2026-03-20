7ef9bcd499274db8187214e929b2251dc9ac4960
Sekaus <christian.foged.ludvigsen@hotmail.com>
2026-03-20 10:31:04 +0100

    Update inbox.php

--- public/inbox.php
+++ public/inbox.php
@@ -22,7 +22,7 @@
                 <ol id="inbox-content-list">
                   <li class="inbox-content">Content</li>
                   <li class="inbox-content">Content</li>
-                  <li class="inbox-content selected-inbox">Content</li>
+                  <li class="inbox-content">Content</li>
                   <li class="inbox-content">Content</li>
                   <li class="inbox-content">Content</li>
                 </ol>
@@ -32,27 +32,68 @@
                 <p id="inbox-name">Note name ▼</p>
 
                 <div id="selected-inbox-content">
-                  <div id="note-top">
-                    {UserMetadata()}
-                    <div class="note-text">
-                      <!-- Note answer here -->
+                  <div id="note-content">
+                    <div id="note-top">
+                      <!-- User metadata here -->
+                      <p id="note-title">Select a note to view its content.</p>
+                      <div class="note-text">
+                        <!-- Note answer here -->
+                      </div>
+                    </div>
+
+                    <br/>
+                    <hr/>
+                    <br/>
+
+                    <div id="note-bottom">
+                      <!-- User metadata here -->
+                      <div class="note-text">
+                          <!-- Original note here --> 
+                      </div>
                     </div>
                   </div>
 
-                  <br/>
-                  <hr/>
-                  <br/>
-
-                  <div id="note-bottom">
-                    {UserMetadata()}
-                    <div class="note-text">
-                        <!-- Original note here --> 
-                    </div>
+                  <div id="watching-content">
+                    <!-- Watching content here -->
+                    <p>Select a watching item to view its content.</p>
                   </div>
                 </div>
             </div>
         </div>
 
         <?php include_once("./html_elements/footer.html"); ?>
+        
+        <script>
+          const inboxFilter = {
+            "notes": 0,
+            "watching": 1
+          };
+
+          let selectedInboxFilter = inboxFilter.notes;
+
+          $(document).ready(function() {
+            $("#watching-content").hide();
+
+            $(".inbox-filter").click(function() {
+              $(".inbox-filter").removeClass("selected-inbox-filter");
+              $(this).addClass("selected-inbox-filter");
+
+              if ($(this).text() === "Notes") {
+                selectedInboxFilter = inboxFilter.notes;
+                $("#note-content").show();
+                $("#watching-content").hide();
+              } else {
+                selectedInboxFilter = inboxFilter.watching;
+                $("#note-content").hide();
+                $("#watching-content").show();
+              }
+            });
+
+            $(".inbox-content").click(function() {
+              $(".inbox-content").removeClass("selected-inbox");
+              $(this).addClass("selected-inbox");
+            });
+          });
+        </script>
     </body>
 </html>

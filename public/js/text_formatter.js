3cb613ad59610fdab5d811d0cca47182ed80917e
Sekaus <christian.foged.ludvigsen@hotmail.com>
2026-05-26 15:36:02 +0200

    Update text_formatter.js

--- public/js/text_formatter.js
+++ public/js/text_formatter.js
@@ -4,3 +4,9 @@ import presetHTML5 from "https://esm.sh/@bbob/preset-html5";
 export function BBCodeRender(text) {
     return bbobHTML(text, presetHTML5());
 }
+
+export function numberFormatter(num) {
+  if (num >= 1000000) return (num / 1000000).toFixed(1) + 'M';
+  if (num >= 1000) return (num / 1000).toFixed(1) + 'K';
+  return num;
+};
